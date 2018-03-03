031b0f3d39ace9f07b2f29f4a474f74f63cd5b82
HL <HL@hl-mba.local>
2018-03-03 14:40:32 -0500

    Initial Commit

---
+++ TodoListCategoryPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoListCategoryPractice
+//
+//  Created by HL on 2018-03-03.
+//  Copyright © 2018 Haolinzhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
